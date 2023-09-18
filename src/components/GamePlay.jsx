f45c672c28e9c8b14d795e403227ec5740c24bd1
f0b <francisefobi@gmail.com>
2023-09-18 22:23:02 +0100

    optimized for desktop view

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -65,21 +65,21 @@ export default function GamePlay() {
     navigate('/')
   }
   
-  const btnPosition = `m-auto w-32 h-32 z-10`
-  const imgBgSize = `!w-[85px] !h-[85px]`
-  const imgHeight = `!h-12`
+  const btnPosition = `m-auto w-32 h-32 z-10 lg:w-60 lg:h-60`
+  const imgBgSize = `!w-[85px] !h-[85px] lg:!w-44 lg:!h-44`
+  const imgHeight = `!h-12 lg:!h-24`
 
   return (
-    <div className='flex flex-col justify-between h-[50vh] font-Barlow'>
+    <div className='flex flex-col justify-between h-[50vh] font-Barlow lg:absolute lg:top-[35%] lg:flex-row lg:items-center'>
       <div className='flex gap-16 tracking-widest'>
-        <div className='flex flex-col items-center justify-center'>
-          <div className='w-32 h-32 rounded-full bg-[hsl(237,49%,15%)]/50 mb-8 relative flex'>
+        <div className='flex flex-col items-center justify-center lg:flex-col-reverse'>
+          <div className='w-32 h-32 rounded-full bg-[hsl(237,49%,15%)]/50 mb-8 relative flex lg:w-60 lg:h-60'>
 
             {/* show overlay when user wins */}
             {result === 'YOU WIN' && (
-              <div className=" group absolute inset-y-0 left-auto -right-[64px] m-auto w-64 h-64 bg-white/5 rounded-full flex justify-center items-center">
-                <div className=" w-56 h-56 bg-white/5 rounded-full flex justify-center items-center">
-                  <div className=" w-44 h-44 bg-white/5 rounded-full"></div>
+              <div className=" group absolute inset-y-0 left-auto -right-[64px] m-auto w-64 h-64 bg-white/5 rounded-full flex justify-center items-center lg:w-[40rem] lg:h-[40rem] lg:-right-[200px]">
+                <div className=" w-56 h-56 bg-white/5 rounded-full flex justify-center items-center lg:w-[30rem] lg:h-[30rem]">
+                  <div className=" w-44 h-44 bg-white/5 rounded-full lg:w-[20rem] lg:h-[20rem]"></div>
                 </div>
               </div>
             )}
@@ -92,17 +92,25 @@ export default function GamePlay() {
               imgSize={`${imgHeight}`}
             />
           </div>
-          <span>YOU PICKED</span>
+          <span className='lg:mb-16 lg:text-xl'>YOU PICKED</span>
         </div>
 
-        <div className='flex flex-col items-center'>
-          <div className='w-32 h-32 rounded-full bg-[hsl(237,49%,15%)]/50 mb-8 relative flex'>
+        <div className='hidden lg:flex flex-col justify-center items-center text-center tracking-normal mt-10 z-10'>
+          {result ? (
+            <div>
+              <h1 className='text-6xl mb-5 font-bold lg:text-5xl'>{result}</h1>
+              <button onClick={playAgain} className='bg-white text-[hsl(229,25%,31%)] text-lg tracking-widest rounded-lg py-4 px-14'>PLAY AGAIN</button>
+            </div>) : (<div>Thinking...</div>)}
+        </div>
+
+        <div className='flex flex-col items-center lg:flex-col-reverse'>
+          <div className='w-32 h-32 rounded-full bg-[hsl(237,49%,15%)]/50 mb-8 relative flex lg:w-60 lg:h-60'>
 
             {/* show overlay when user loses */}
             {result === 'YOU LOSE' && (
-              <div className=" group absolute inset-y-0 left-auto -right-[64px] m-auto w-64 h-64 bg-white/5 rounded-full flex justify-center items-center">
-                <div className=" w-56 h-56 bg-white/5 rounded-full flex justify-center items-center">
-                  <div className=" w-44 h-44 bg-white/5 rounded-full"></div>
+              <div className=" group absolute inset-y-0 left-auto -right-[64px] m-auto w-64 h-64 bg-white/5 rounded-full flex justify-center items-center lg:w-[40rem] lg:h-[40rem] lg:-right-[200px]">
+                <div className=" w-56 h-56 bg-white/5 rounded-full flex justify-center items-center lg:w-[30rem] lg:h-[30rem]">
+                  <div className=" w-44 h-44 bg-white/5 rounded-full lg:w-[20rem] lg:h-[20rem]"></div>
                 </div>
               </div>
             )}
@@ -114,11 +122,11 @@ export default function GamePlay() {
               imgSize={`${imgHeight}`}
             />
           </div>
-          <span className=' whitespace-nowrap'>THE HOUSE PICKED</span>
+          <span className='whitespace-nowrap lg:mb-16 lg:text-xl'>THE HOUSE PICKED</span>
         </div>
       </div>
 
-      <div className='flex flex-col justify-center items-center text-center'>
+      <div className='lg:hidden flex flex-col justify-center items-center text-center'>
         {result ? (
           <div>
             <h1 className='text-6xl mb-5 font-bold'>{result}</h1>
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -18,11 +18,11 @@ export default function RootLayout() {
     }
 
     return (
-        <div className=' sm:flex sm:flex-col h-screen bg-gradient-to-b from-[hsl(214,47%,23%)] to-[hsl(237,49%,15%)]'>
-            <div className=' container mx-auto flex flex-col h-screen justify-between'>
-                <header className='flex justify-between items-center outline outline-4 outline-[hsl(217,16%,45%)] rounded-lg mt-5 mb-5 p-2'>
-                    <img src={logobonus} alt="logobonus" className='h-16 ml-5'/>
-                    <div className='bg-white flex justify-center w-24 h-24 rounded-lg'>
+        <div className=' sm:flex sm:flex-col h-screen bg-gradient-to-b from-[hsl(214,47%,23%)] to-[hsl(237,49%,15%)] lg:relative lg:overflow-hidden'>
+            <div className=' container mx-auto flex flex-col h-screen justify-between lg:max-w-3xl'>
+                <header className='flex justify-between items-center outline outline-4 outline-[hsl(217,16%,45%)] rounded-lg mt-5 mb-5 p-2 lg:p-5 lg:mt-10 lg:outline-2 lg:mx-10'>
+                    <img src={logobonus} alt="logobonus" className='h-16 ml-5 lg:h-auto'/>
+                    <div className='bg-white flex justify-center w-24 h-24 rounded-lg lg:w-32 lg:h-[115px]'>
                         <ScoreBoard/>
                     </div>
                 </header>
@@ -31,16 +31,32 @@ export default function RootLayout() {
                     <Outlet/>
                 </main>
 
-                <footer className=' flex justify-center text-white pb-20 font-Barlow'>
-                    <button onClick={handleClickOpen} className=' border-2 border-white py-2 px-10 rounded-lg tracking-widest text-lg'>RULES</button>
-                    {isClicked && 
-                        (<div className='flex flex-col items-center justify-between h-screen w-full absolute top-0 bg-white pb-24 z-30'>
-                            <h1 className='text-[hsl(229,25%,31%)] text-4xl mt-20 font-bold'>RULES</h1>
-                            <img src={imagerules} alt="image-rules-bonus" />
-                            <button onClick={handleClickClose}>
-                                <img src={iconclose} alt="icon-close" />
-                            </button>
-                        </div>)}
+                <footer className=' flex justify-center text-white pb-20 font-Barlow '>
+                    <button onClick={handleClickOpen} className=' border-2 border-white py-2 px-10 rounded-lg tracking-widest text-lg lg:absolute lg:bottom-0 lg:right-0 lg:mb-10 lg:mr-10'>RULES</button>
+                    {isClicked && (
+                        <>
+                            <div className='lg:hidden flex flex-col items-center justify-between h-screen w-full absolute top-0 bg-white pb-24 z-30'>
+                                <h1 className='text-[hsl(229,25%,31%)] text-4xl mt-20 font-bold'>RULES</h1>
+                                <img src={imagerules} alt="image-rules-bonus" />
+                                <button onClick={handleClickClose}>
+                                    <img src={iconclose} alt="icon-close" />
+                                </button>
+                            </div>
+
+                            {/* overlay */}
+                            <div className='hidden lg:block w-full h-screen bg-black/50 absolute inset-0 z-30'></div>
+
+                            <div className='hidden lg:flex flex-col absolute top-36 bottom-40 bg-white px-6 py-5 rounded-lg z-30'>
+                                <div className=' flex justify-between mb-5'>
+                                    <span className='text-[hsl(229,25%,31%)] text-3xl font-bold'>RULES</span>
+                                    <button onClick={handleClickClose}>
+                                        <img src={iconclose} alt="icon-close" />
+                                    </button>
+                                </div>
+                                <img src={imagerules} alt="image-rules-bonus" />
+                            </div>
+                        </>
+                    )}
                 </footer>
             </div>
         </div>
